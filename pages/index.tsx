c18c935a93676fc71a77cc0af718d5356ba17c41
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-01-25 22:19:45 +0400

    :boom: initial

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ContactProposal from './components/index_components/contact-proposal';
+import Landing from './components/index_components/landing';
+
+
+const index = () => {
+  return (
+    <>
+      <Landing/>
+      <ContactProposal/>
+    </>
+  );
+};
+
+export default index;
